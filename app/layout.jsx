408ed1262335acef0fa4db20cc2c6c1060c0d400
Zaryab Khan <zaryabcom123@gmail.com>
2024-01-12 02:41:37 +0500

    Setup of  Navbar and Intro Section

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import Header from "@/components/Header";
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -11,10 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-50 text-gray-950`}>
-      <div className='bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[11rem] h-[31.24rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
+      <body className={`${inter.className} bg-gray-50 text-gray-950 pt-8 sm:pt-12`}>
+      <div className='bg-[#fbe23] absolute -z-10 top-[-6rem] right-[11rem] h-[31.24rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
       <div className='bg-[#dbd7fb] -z-10 absolute top-[-1rem] left-[-35rem] h-[31.24rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl-left[-15rem] 2xl:left-[-5rem]'></div>
+      <Header/>
       {children}
+      {/* <Footer/> */}
       </body>
     </html>
   )
--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,10 @@
+import Intro from "@/components/Intro";
 
-export default function Home() {
+const Home =() => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
+      <Intro/>
     </main>
   )
 }
+export default Home
